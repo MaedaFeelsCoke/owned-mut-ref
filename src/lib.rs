82c14427b65241a9103182497634b81ddf34eb58
Maeda <=>
2025-08-13 19:07:53 -0400

    added async support

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,51 @@
-mod oneshot;
-use std::ops::{Deref, DerefMut};
+use std::{
+    ops::{Deref, DerefMut},
+    pin::Pin,
+    sync::{Arc, Condvar, Mutex},
+    task::{Context, Poll, Waker},
+};
 
-#[repr(C)]
 #[derive(Debug)]
-pub struct OwnedMutRefWaiter<'a, T: Send>(oneshot::Receiver<()>, &'a mut T);
+#[allow(dead_code)] // suppress the warning about the unused &'a mut T
+pub struct OwnedMutRefWaiter<'a, T: Send>(Arc<(Condvar, Mutex<(Option<Waker>, bool)>)>, &'a mut T);
 
 impl<T: Send> OwnedMutRefWaiter<'_, T> {
     pub fn wait(self) {
-        let (done, _ui): (oneshot::Receiver<()>, &mut T) = unsafe { std::mem::transmute(self) };
-        done.recv();
+        #[allow(let_underscore_lock)]
+        let _ = self.0.0.wait_while(self.0.1.lock().unwrap(), |x| !x.1);
+    }
+    /// returns true if the waiter is ready to be dropped, false otherwise
+    pub fn try_wait(&self) -> bool {
+        self.0.1.lock().unwrap().1
+    }
+}
+
+impl<T: Send> Future for OwnedMutRefWaiter<'_, T> {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut lock = self.0.1.lock().unwrap();
+        if lock.1 {
+            Poll::Ready(())
+        } else {
+            lock.0 = Some(cx.waker().clone());
+            Poll::Pending
+        }
     }
 }
 
 impl<T: Send> Drop for OwnedMutRefWaiter<'_, T> {
     fn drop(&mut self) {
-        eprintln!("OwnedMutRefWaiter needs to be waited on");
-        std::process::abort();
+        if !self.0.1.lock().unwrap().1 {
+            eprintln!("OwnedMutRefWaiter needs to be waited on");
+            std::process::abort();
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct OwnedMutRef<T: Send> {
-    _done: oneshot::Sender<()>,
+    done: Arc<(Condvar, Mutex<(Option<Waker>, bool)>)>,
     value: *mut T,
 }
 
@@ -43,7 +67,12 @@ impl<T: Send> DerefMut for OwnedMutRef<T> {
 }
 
 impl<T: Send> Drop for OwnedMutRef<T> {
-    fn drop(&mut self) {}
+    fn drop(&mut self) {
+        let mut lock = self.done.1.lock().unwrap();
+        lock.1 = true;
+        lock.0.take().map(|waker| waker.wake());
+        self.done.0.notify_one();
+    }
 }
 
 impl<T: Send> OwnedMutRef<T> {
@@ -52,6 +81,8 @@ impl<T: Send> OwnedMutRef<T> {
     /// use std::sync::mpsc;
     ///
     /// fn main() {
+    ///     // WARNING: KILLING THE THREAD THAT OWNS THE MUTABLE REFERENCE IS UB
+    ///
     ///     // create a mutable reference
     ///     let mut x = 1;
     ///     let mut_ref = &mut x;
@@ -71,21 +102,24 @@ impl<T: Send> OwnedMutRef<T> {
     ///     drop(received_owned_mut);
     ///
     ///     // you must wait on the waiter
-    ///     // dropping the waiter will abort the program
+    ///     // dropping the waiter before the owned_mut is dropped will std::process:abort
     ///     waiter.wait();
+    ///     // for async code
+    ///     //waiter.await
     ///
     ///     // once you called wait the value is free to be used again
     ///     println!("{x}");
     /// }
     ///```
+
     pub fn new(value: &mut T) -> (Self, OwnedMutRefWaiter<T>) {
-        let (done_tx, done_rx) = oneshot::channel();
+        let done = Arc::new((Condvar::new(), Mutex::new((None, false))));
         (
             Self {
-                _done: done_tx,
+                done: done.clone(),
                 value: value as *mut T,
             },
-            OwnedMutRefWaiter(done_rx, value),
+            OwnedMutRefWaiter(done, value),
         )
     }
 }
